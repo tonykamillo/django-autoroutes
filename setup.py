06fde25051393bd1e05a41f6e24df25cfbc725c0
Tony <tony@hitman.(none)>
2012-06-15 18:06:34 -0300

    Adding instalations script

---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name = 'django-autoroutes',
+    description = 'Module to automate routes for django projects',
+    version = '0.1.0',
+    author = 'Tony Kamillo Borges Reis',
+    author_email = 'tonysacanix@gmail.com',
+    maintainer = 'Tony Kamillo Borges Reis',
+    maintainer_email = 'tonysacanix@gmail.com',
+    url = 'https://bitbucket.org/rodrigopmatias/monitor/get/tip.tar.gz',    
+    install_requires = ['django',]
+)
